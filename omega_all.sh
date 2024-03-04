d10e054db47d58f823325d767151ddb6491f41fc
Pau Roldan <roldan.pau@gmail.com>
2024-03-04 16:31:18 +0100

    Minor change in omega_all.sh

--- omega_all.sh
+++ omega_all.sh
@@ -1,6 +1,7 @@
 PAPERDIR=~/mat-nuvol/research/ndft-paper
 IMGDIR=$PAPERDIR/images_SM2
 
+make
 ./omega 1.0 > omega.res 2> omega_all.res
 ./omega 2.0 > omega.res 2>> omega_all.res
 ./omega 3.0 > omega.res 2>> omega_all.res
