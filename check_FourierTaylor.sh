9ad2e6680b99c3d49153cdad7db7eb94adede04a
Pau Roldan <roldan.pau@gmail.com>
2025-05-07 08:24:58 +0200

    Added 7 new curves

--- check_FourierTaylor.sh
+++ check_FourierTaylor.sh
@@ -1,3 +1,9 @@
+# check_FourierTaylor.sh: Plot image of the numerical SM versus SSM.
+#
+# This script is used both for N=L=2, and N=4, L=5. Before calling this script,
+# make sure to set degrees N,L in SM_given_I.c. Also, make sure to modify last
+# line of this script.
+
 PAPERDIR=~/research/ndft-paper
 
 make SM_given_I
@@ -10,4 +16,5 @@ do
 done
 
 gnuplot check_FourierTaylor.plt
-mv check_FourierTaylor.tex $PAPERDIR/images
+mv check_FourierTaylor.tex $PAPERDIR/images/check_FourierTaylor_N2_M2.tex
+#mv check_FourierTaylor.tex $PAPERDIR/images/check_FourierTaylor_N4_M5.tex
